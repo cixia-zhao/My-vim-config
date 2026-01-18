76e455ecae046baf4f1654336b7aeb97cbd6e152
cixia-zhao <2135952859@qq.com>
2026-01-18 15:36:42 +0800

    feat: Finalize config, fix Tree-sitter loop

--- lua/plugins/extras/lang/cpp.lua
+++ lua/plugins/extras/lang/cpp.lua
@@ -1,17 +1,34 @@
+-- ~/.config/nvim/lua/plugins/extras/lang/cpp.lua
+-- 这是我们C++环境的最终、统一配置文件
+
 return {
-  -- 禁用 mason-lspconfig 的自动安装功能
+  -- 1. 修正 Mason：告诉它不要多管闲事去装 clangd
   {
     "williamboman/mason-lspconfig.nvim",
     opts = {
       ensure_installed = {},
     },
   },
-  -- 明确告诉 lspconfig 我们需要 clangd 服务器
+
+  -- 2. 修正 LSP Config：明确告诉它我们需要 clangd 服务器
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         clangd = {},
+      },
+    },
+  },
+
+  -- 3. 修正 Tree-sitter：让它只关心 C 和 C++
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "c",
+        "cpp",
+        -- 我们只关心 C 和 C++，这可以防止每次启动都去尝试安装
+        -- 其他几十个我们用不到的、且可能导致失败的解析器。
       },
     },
   },
